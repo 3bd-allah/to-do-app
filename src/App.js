4e8cee903aed09fdea06dba3e6318214e5e3f460
3bd-allah <abdullah.nabil5044@gmail.com>
2023-09-06 20:59:13 +0300

    use React-router

--- src/App.js
+++ src/App.js
@@ -2,40 +2,38 @@ import { useState } from "react";
 import "./App.css";
 import NavbarComponent from "./components/Navbar";
 import TodoForm from "./components/TodoForm";
-import Todos from "./components/Todos"
+import Todos from "./components/Todos";
 function App() {
-  const [todos , setTodos] = useState([]);
+  const [todos, setTodos] = useState([]);
 
-  function getFormValue(data){
-    newTodo(data)
+  function getFormValue(data) {
+    newTodo(data);
   }
 
-  const newTodo = (todoTitle) =>{
-    if(todoTitle ===""){
-      alert("Enter your task first!")
-      return 
-    }else if (todos.find((todo) => todo.title.trim() === todoTitle.trim())){
-      return 
-    }
-
-    else{
-
+  const newTodo = (todoTitle) => {
+    if (todoTitle === "") {
+      alert("Enter your task first!");
+      console.log("Error!");
+      return;
+    } else if (todos.find((todo) => todo.title.trim() === todoTitle.trim())) {
+      return;
+    } else {
       const todo = {
-        id :Date.now() , 
-        title:todoTitle , 
-        done :false
-      }
-      const allTodos = [...todos , todo]
-      setTodos (allTodos)
+        id: Date.now(),
+        title: todoTitle,
+        done: false,
+      };
+      const allTodos = [...todos, todo];
+      setTodos(allTodos);
     }
-  }
+  };
   return (
     <div className="App">
       <NavbarComponent />
       <div className="mt-5 d-flex justify-content-center ">
         <div className="border m-3 p-5 w-50 rounded-4">
           <h3 className="mb-5">Make Things Done!</h3>
-          <TodoForm onSubmit ={getFormValue} />
+          <TodoForm onSubmit={getFormValue} />
           <Todos todos={todos} />
         </div>
       </div>
